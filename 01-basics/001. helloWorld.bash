36881341c59b5c2d40ef21a2d243795c13c9dbd3
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2022-03-19 00:05:08 +0530

    Create 001. helloWorld.bash

---
+++ 01-basics/001. helloWorld.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Author : Krushna
+# Date Created : 19/03/22
+# Description : This script will give output "Heelo World" on the screen
+# Date Modified : 19/03/22
+
+echo
+echo Hello World
+echo
