799c1156a3b39fbc8fe10281c2b31f5a765dddf1
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2022-03-20 00:02:52 +0530

    Create 002. basictasks.sh

---
+++ 01-basics/002. basictasks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Author : Krushna
+# Date Created : 20/03/22
+# Description : This script will run a few basic commands.
+# Date Modified : 20/03/22
+
+echo
+echo This script will run a few basic commands.
+echo
+pwd
+ls
+whoami
+date
+cal
+touch a b c
