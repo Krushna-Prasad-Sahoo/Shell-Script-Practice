acb1d0793a5680ff365ffe1bcb9d45b7f4573756
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2022-03-20 00:04:26 +0530

    Create 003. admintasks.sh

---
+++ 01-basics/003. admintasks.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Author : Krushna
+# Date Created : 20/03/22
+# Description : This script will run basic admin commands .
+# Date Modified : 20/03/22
+
+echo
+echo This script will run basic admin commands .
+echo
+top
+echo
+df -h
+echo
+free -m
+echo
+uptime
+echo
+iostat
+echo
+echo End of script
