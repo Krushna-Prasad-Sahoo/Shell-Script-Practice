cab5574fedc6f1c70ab09c9b16eade273bbe4b95
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2022-03-21 22:10:47 +0530

    Create 007. numCheck-ifElse.sh

---
+++ 01-basics/007. numCheck-ifElse.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Author : Krushna
+# Date Created :21/03/22
+# Description : Simple If-else use case
+# Date Modified : 21/03/22
+
+num=10
+if [ $num -eq 100 ]
+then 
+	echo Number is 100.
+else
+	echo Number is not 100.
+fi
