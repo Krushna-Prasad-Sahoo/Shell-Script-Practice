69ba41e4e30ce08d6b9af7eb8b89b261f5c83a49
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2022-03-21 10:26:11 +0530

    Create 005. basic-input-script.sh

---
+++ 01-basics/005. basic-input-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Author : Krushna
+# Date Created : 21/03/22
+# Description : Input test
+# Date Modified : 21/03/22
+
+echo Hello, I am KP 
+echo
+echo What is your name ?
+read n
+echo Hello $n, have a nide day !!
