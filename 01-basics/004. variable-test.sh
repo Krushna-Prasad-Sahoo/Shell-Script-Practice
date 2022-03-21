f6d93661d27094c0d624ed036e6300ae453ba502
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2022-03-21 10:16:56 +0530

    Create 004. variable-test.sh

---
+++ 01-basics/004. variable-test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Author : Krushna
+# Date Created : 21/03/22
+# Description : Define & test variables
+# Date Modified : 21/03/22
+
+# variable definition
+p=pwd
+l=ls
+wi=whoami
+d=date
+c='cal 2020'
+
+# test variables
+echo Run variables tasks
+echo
+$p
+$l
+$wi
+$d
+$c
