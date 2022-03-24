78a3daffa778b5e992ac7c7681601ee54ce138f1
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2022-03-24 20:20:36 +0530

    Create 011. forLoop-ex1.sh

---
+++ 01-basics/011. forLoop-ex1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Author : Krushna
+# Date Created : 24/03/22
+# Description : Simple For Loop Example
+# Date Modified : 24/03/22
+
+for i in 1 2 3 4 5
+do
+        echo Welcome $i time
+done
