069cdff15f8d5fc61ab9e3f3bcdc5f9c34455e8c
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2022-03-24 20:41:52 +0530

    Create 013. forLoop-fileCreation.sh

---
+++ 01-basics/013. forLoop-fileCreation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Author : Krushna
+# Date Created : 24/03/22
+# Description : Using For Loop for file creation
+# Date Modified : 24/03/22
+
+for i in {1..5}
+do
+        touch $i
+done
