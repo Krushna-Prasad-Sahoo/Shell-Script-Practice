a0cdb5c47fc256fa34eb395318b97e2c207756ed
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2022-03-24 20:29:57 +0530

    Create 012. forLoop-ex2.sh

---
+++ 01-basics/012. forLoop-ex2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Author : Krushna
+# Date Created : 24/03/22
+# Description : Simple For Loop Example Two
+# Date Modified : 24/03/22
+
+clear
+for i in Cricket Pizza Hiking Gaming travelling photography
+do
+        echo I like $i
+        echo ------
+done
