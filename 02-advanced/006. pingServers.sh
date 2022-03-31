fa88b768f7589b5f459ceae03d43d15c727258b8
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2022-03-31 21:24:15 +0530

    Create 006. pingServers.sh

---
+++ 02-advanced/006. pingServers.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Author : Krushna
+# Date created : 31/03/2022
+# Description : 
+# Date modified : 31/03/2022
+
+ping -c1 192.168.1.1
+
+if [ $? -eq 0 ]
+then
+	echo OKAY
+else
+	echo NOT OKAY
+fi
