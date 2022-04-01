521cd6417a58a67888537d712de9ca574bca83b5
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2022-04-01 17:07:13 +0530

    Update 009. pingServers-crontab.sh

--- 02-advanced/009. pingServers-crontab.sh
+++ 02-advanced/009. pingServers-crontab.sh
@@ -4,6 +4,11 @@
 # Date Modified : 01/04/2022
 
 
-# press `crontab -e` for editing the cron file.
+### press `crontab -e` for editing the cron file.
 
+### create a output file
 # 34 20 * * *  /home/student/pingServers-variable-for.sh  >  ping-output
+
+
+### for mailing
+# 34 20 * * *  /home/student/pingServers-variable-for.sh  >  mail -s 'Connectivity Status' test@hotmail.com
