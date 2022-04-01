8c322bdba6193d50f1392076e57d26e6e59dc1b0
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2022-04-01 17:03:23 +0530

    Create 009. pingServers-crontab.sh

---
+++ 02-advanced/009. pingServers-crontab.sh
@@ -0,0 +1,9 @@
+# Author : Krushna
+# Date Created : 01/04/2022
+# Description : 
+# Date Modified : 01/04/2022
+
+
+# press `crontab -e` for editing the cron file.
+
+# 34 20 * * *  /home/student/pingServers-variable-for.sh  >  ping-output
