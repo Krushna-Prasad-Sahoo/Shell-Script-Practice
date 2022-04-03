8ab7014ee42bbcb6bb0cd734f411564aff94b4ca
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2022-04-03 17:44:35 +0530

    Update 008. pingServers-variable-for.sh

--- 02-advanced/008. pingServers-variable-for.sh
+++ 02-advanced/008. pingServers-variable-for.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Author : Krushna
 # Date created : 31/03/2022
-# Description : 
-# Date modified : 31/03/2022
+# Description : check if the servers listed out in the file is active & can reply a ping request or not
+# Date modified : 03/04/2022
 # The file /home/student/myhosts contains a list of IPs
 
 hosts="/home/student/myhosts"
