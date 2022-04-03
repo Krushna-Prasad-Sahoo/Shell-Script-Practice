a745eaa07c169d7ac0f96f2efe49d37f2fccc6bf
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2022-04-03 17:52:38 +0530

    Update 007. pingServers-variable.sh

--- 02-advanced/007. pingServers-variable.sh
+++ 02-advanced/007. pingServers-variable.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Author : Krushna
 # Date created : 31/03/2022
-# Description : 
-# Date modified : 31/03/2022
+# Description : check if a server is active & can reply a ping request or not
+# Date modified : 03/04/2022
 
 host="192.168.1.1"
 
