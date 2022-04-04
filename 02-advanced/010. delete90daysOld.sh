7c4f78e9e3d2e33fe82b3dd8a237cb2590fbfb43
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2022-04-04 23:04:11 +0530

    Update 010. delete90daysOld.sh

--- 02-advanced/010. delete90daysOld.sh
+++ 02-advanced/010. delete90daysOld.sh
@@ -4,4 +4,4 @@
 # Desciption : This will find & delete 90 days old files
 # Date Modified : 04/04/2022
 
-find  /home/students/kp  -mtime +90  -exec  ls  -l  {} \;
+find  /home/students/kp  -mtime +90  -exec  rm  -f  {} \;
