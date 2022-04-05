bf74837a9f34a74e3463ab343d09a7ddf49eea7e
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2022-04-05 20:00:06 +0530

    Update 012. backupFilesys.sh

--- 02-advanced/012. backupFilesys.sh
+++ 02-advanced/012. backupFilesys.sh
@@ -7,3 +7,15 @@
 tar cvf  /tmp/backup.tar  /etc  /var
 
 gzip /tmp/backup.tar
+
+find  /tmp/backup.tar.gz -mtime -1 -type f -print &> /dev/null
+
+if  [ $? -eq 0 ]
+then
+	echo Backup was created
+	echo
+	echo Archiving Backup
+	scp /tmp/backup.tar.gz  root@192.168.1.2:/var
+else
+	echo Backup Failed
+fi
