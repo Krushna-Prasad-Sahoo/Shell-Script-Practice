3d6830f2cd4a9c1dc766a7388bde931af09f2707
Krushna Prasad Sahoo <krushna.360degree@gmail.com>
2022-06-13 07:32:41 +0530

    Update cpu-temp-check.sh

--- cpu-temp-check.sh
+++ cpu-temp-check.sh
@@ -4,3 +4,6 @@
 
 # minimal way
 cat /sys/class/thermal/thermal_zone*/temp
+
+# preferred way
+paste <(cat /sys/class/thermal/thermal_zone*/type) <(cat /sys/class/thermal/thermal_zone*/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'
